7b0d68d623a89b22192c9f8e60b9dbe901c10e20
yukiya119 <yukinosuke911@gmail.com>
2022-01-28 21:25:03 +0900

    課題Ｂ終了

--- b-6.py
+++ b-6.py
@@ -2,12 +2,12 @@ def main():
 
     import random
 
-    dice_face = int(input("サイコロの面の数は?: "))
-    dice_roll = int(input("何回振りますか?: "))
+    dice_men = int(input("サイコロの面の数は?: "))
+    dice_count = int(input("何回振りますか?: "))
 
     def dice():
-        if dice_face >= 1 and dice_roll >= 1:
-            dice_trial = [random.randint(1, dice_face) for d in range(1, dice_roll + 1)]
+        if dice_men >= 1 and dice_count >= 1:
+            dice_trial = [random.randint(1, dice_men) for d in range(1, dice_count + 1)]
             return dice_trial
         else:
             raise ValueError("正の整数(1以上)を入力してください")
